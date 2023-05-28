4138c98c2b855e46d78bc0c044afc1762ecf99ab
Amjad Projects <aakhan1572@yahoo.com>
2023-05-28 20:58:07 +0530

    first commit

--- expads/models.py
+++ expads/models.py
@@ -1,10 +1,11 @@
+import os
 from django.db import models
 #from django.contrib.auth.models import User
 from accounts.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse
 from sorl.thumbnail import get_thumbnail
-#from django.dispatch import receiver
+from django.dispatch import receiver
 
 from phonenumber_field.modelfields import PhoneNumberField
 #from django.db.models.signals import post_delete
@@ -166,15 +167,15 @@ class Interested(models.Model):
         return self.fullname
 
 
-#import os
-#def _delete_file(path):
-#   """ Deletes file from filesystem. """
-#   if os.path.isfile(path):
-#       os.remove(path)
 
-#@receiver(models.signals.post_delete, sender=ExpatImage)
-#def delete_file(sender, instance, *args, **kwargs):
-#    """ Deletes image files on `post_delete` """
-#    if instance.images:
-#        _delete_file(instance.images.path)       
+def _delete_file(path):
+   """ Deletes file from filesystem. """
+   if os.path.isfile(path):
+       os.remove(path)
+
+@receiver(models.signals.post_delete, sender=ExpatImage)
+def delete_file(sender, instance, *args, **kwargs):
+    """ Deletes image files on `post_delete` """
+    if instance.images:
+        _delete_file(instance.images.path)       
 
