27bbed59e26dd1c67c1729b8bab311853e089fd2
Amjad Projects <aakhan1572@yahoo.com>
2023-05-28 19:53:17 +0530

    first commit

--- main/settings.py
+++ main/settings.py
@@ -42,7 +42,7 @@ DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 #DEBUG = env('DEBUG', cast=bool)
 
 
