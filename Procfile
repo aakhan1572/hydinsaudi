91f50f17e92f7eff51b3143cca7ed546c0f1d467
Amjad Projects <aakhan1572@yahoo.com>
2023-05-29 19:52:36 +0530

    first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main.wsgi  --log-file -
+web: gunicorn --bind 0.0.0.0:8000 main.wsgi
--- main/settings.py
+++ main/settings.py
@@ -255,3 +255,19 @@ else:
 #    print('True')
 #else:
 #    print('False')
+
+"""
+# Following setting for http 
+# python manage.py runserver 8080
+
+DEBUG=True
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
+"""
